548d922ab3ad523d4c680a4797545bc38fc6225f
zawho <zthooper2@gmail.com>
2023-08-30 16:04:41 +0800

    Ass menu create funcs to JS

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ function displayMenu() {
     for (let i = 0; i < thisMenuArr.length; i++) {
         if (thisMenuArr[i].className === 'dropdown-option') {
             thisMenuArr[i].style.display = 'flex';
-            thisMenuArr[i].style.justifyContent = 'center';
-            thisMenuArr[i].style.alignItems = 'center';
         }
     }
 }
@@ -29,6 +27,44 @@ function setMenuEvent() {
     }
 }
 
-setMenuEvent();
+function createMenu() {
+    const body = document.querySelector('body');
+    const dropDownMenu = document.createElement('div');
+    dropDownMenu.className = 'dropdown-menu';
+    dropDownMenu.innerText = 'menu';
+    body.appendChild(dropDownMenu);
+    const dropDownOptions = [];
+    for (let i = 1; i < 6; i++) {
+        dropDownOptions[i] = document.createElement('div');
+        dropDownOptions[i].className = 'dropdown-option';
+        dropDownOptions[i].innerText = `option ${i}`;
+        dropDownMenu.appendChild(dropDownOptions[i]);
+        dropDownOptions[i].style.display = 'none';
+    }
+    setMenuEvent();
+}
 
-export default setMenuEvent;
+function styleMenu() {
+    const dropDownMenus = document.querySelectorAll('.dropdown-menu');
+    const dropDownOptions = document.querySelectorAll('.dropdown-option');
+    for (let i = 0; i < dropDownMenus.length; i++) {
+        dropDownMenus[i].style.textAlign = 'center';
+        dropDownMenus[i].style.display = 'flex';
+        dropDownMenus[i].style.flexDirection = 'column';
+        dropDownMenus[i].style.border = '1px solid black';
+        dropDownMenus[i].style.maxWidth = '200px';
+        dropDownMenus[i].style.gap = '5px';
+        dropDownMenus[i].style.padding = "5px 0px";
+    }
+    for (let i = 0; i < dropDownOptions.length; i++) {
+        dropDownOptions[i].style.display = 'none';
+        dropDownOptions[i].style.borderTop = '1px solid black';
+        dropDownOptions[i].style.paddingTop = '5px';
+        dropDownOptions[i].style.justifyContent = 'center';
+        dropDownOptions[i].style.alignItems = 'center';
+    }
+}
+createMenu();
+styleMenu();
+
+export { createMenu, styleMenu };
